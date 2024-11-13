484323988c30785344cd79c0c4a3f0d14fc7245f
Giulio Yongco <130813239+GM-Yongco@users.noreply.github.com>
2024-11-13 22:36:10 +0800

    i give up,  it wont compile

--- year3_sem1/archi/clear_screen.asm
+++ year3_sem1/archi/clear_screen.asm
@@ -1,13 +1,12 @@
-
-MOV AL, ' '
+MOV AL, ' '					; Set to nonbreaking space
 mov DS, 0b800h				; Set DS to the video memory segment
 
 mov SI, 0					; initialize
-DL_character_in_DS_SI:
+loop:
 	mov b[DS:SI], AL		; using byte offsets, in the video memory segmet, offset of value si by label/offset of byte then put al in there
 
 add SI, 2					; itterate
 cmd SI, 4000				; comparison
-jg DL_character_in_DS_SI	; jump if greater
+jg loop						; jump if greater
 
 INT 020
